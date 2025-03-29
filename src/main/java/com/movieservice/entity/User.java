c07c02ba4984050f3c998af0c480cb5ed40263fd
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-03-29 23:51:16 +0530

    add the columns

--- src/main/java/com/movieservice/entity/User.java
+++ src/main/java/com/movieservice/entity/User.java
@@ -2,6 +2,8 @@ package com.movieservice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,8 +12,12 @@ import jakarta.persistence.Table;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column( nullable = false, unique = true) 
     private Long id;
+    
+    @Column(nullable=false)
+    private String name;
 
     @Column(name = "email", nullable = false, unique = true) 
     private String email;
@@ -19,27 +25,36 @@ public class User {
     @Column(name = "password", nullable = false) 
     private String password;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
