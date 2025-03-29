cebee7d5d3d916c3a516ca5edef1b955cc9e3889
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-03-29 23:57:38 +0530

    added insert tables and also alter

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -6,3 +6,16 @@ CREATE TABLE movietick.users (
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+ALTER TABLE movietick.users
+ADD COLUMN name VARCHAR(255) NOT NULL;
+
+CREATE TABLE movietick.movies (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    language VARCHAR(50) NOT NULL,
+    duration INT NOT NULL,  
+    description TEXT,
+    image_url VARCHAR(500)
+);
