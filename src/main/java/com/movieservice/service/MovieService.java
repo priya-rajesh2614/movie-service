8308dfc4a4253890996e0f346a858be1f8624e93
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-03-30 22:08:26 +0530

    add the get theater by movie

--- src/main/java/com/movieservice/service/MovieService.java
+++ src/main/java/com/movieservice/service/MovieService.java
@@ -4,9 +4,11 @@ package com.movieservice.service;
 import org.springframework.stereotype.Service;
 
 import com.movieservice.entity.Movie;
+import com.movieservice.entity.Theater;
 import com.movieservice.repository.MovieRepository;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class MovieService {
@@ -20,5 +22,10 @@ public class MovieService {
 	public List<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
+
+	public Set<Theater> getTheatersByMovie(Long movieId) {
+		 Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new RuntimeException("Movie not found"));
+	        return movie.getTheaters();
+	}
 
 }
