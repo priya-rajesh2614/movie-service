cae26992a78dab1d0bae3e9903dab186ea489b48
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-03-30 22:12:01 +0530

    add the getmapping  get theater by movie

--- src/main/java/com/movieservice/controller/MovieController.java
+++ src/main/java/com/movieservice/controller/MovieController.java
@@ -3,9 +3,11 @@ package com.movieservice.controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.movieservice.entity.Movie;
+import com.movieservice.entity.Theater;
 import com.movieservice.service.MovieService;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/movies")
@@ -21,6 +23,11 @@ public class MovieController {
     public List<Movie> getAllMovies() {
         return movieService.getAllMovies();
     }
+	
+	 @GetMapping("/{movieId}/theaters")
+	    public Set<Theater> getTheatersByMovie(@PathVariable Long movieId) {
+	        return movieService.getTheatersByMovie(movieId);
+	    }
 
 
    
