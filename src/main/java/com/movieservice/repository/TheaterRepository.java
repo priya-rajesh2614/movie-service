17346d4da03182b6bd267c1a956866d2dfb5cd72
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-03-30 22:03:32 +0530

    create api request for theaters

---
+++ src/main/java/com/movieservice/repository/TheaterRepository.java
@@ -0,0 +1,9 @@
+package com.movieservice.repository;
+
+import com.movieservice.entity.Theater;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TheaterRepository extends JpaRepository<Theater, Long> {
+}
