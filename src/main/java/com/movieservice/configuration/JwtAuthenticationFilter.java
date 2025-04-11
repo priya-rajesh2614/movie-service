3481e7cdbdb8e29f255e1f74641b08fecad15b54
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-04-11 11:30:31 +0530

    token generator in jwtauthenticationfilter i added some changes

--- src/main/java/com/movieservice/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/movieservice/configuration/JwtAuthenticationFilter.java
@@ -2,21 +2,60 @@ package com.movieservice.configuration;
 
 import java.io.IOException;
 
-import jakarta.servlet.Filter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.movieservice.json.JwtUtil;
+import com.movieservice.service.UserDetailsServiceImpl;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtAuthenticationFilter implements Filter {
-	
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+	private JwtUtil jwtUtil;
+	private UserDetailsServiceImpl userDetailsServiceImpl;
+
+	public JwtAuthenticationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsServiceImpl) {
+		this.jwtUtil = jwtUtil;
+		this.userDetailsServiceImpl = userDetailsServiceImpl;
+	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		// TODO Auto-generated method stub
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws ServletException, IOException {
 
+		String authHeader = request.getHeader("Authorization");
+
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		String token = authHeader.substring(7);
+		
+		String emailId = jwtUtil.extractEmail(token);
+
+		if (emailId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(emailId);
+
+			if (jwtUtil.validateToken(token, userDetails)) {
+				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+						userDetails, null, userDetails.getAuthorities());
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		}
+
+		chain.doFilter(request, response);
 	}
 
 }
