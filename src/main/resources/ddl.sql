bf7b47971677dc2ff60b3063ab6636017f1f8e4f
priya-rajesh2614 <priya.rajesh26143@gmail.com>
2025-04-11 11:38:19 +0530

    i added some changes in DLL

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -10,6 +10,9 @@ CREATE TABLE movietick.users (
 ALTER TABLE movietick.users
 ADD COLUMN name VARCHAR(255) NOT NULL;
 
+ALTER TABLE movietick.users
+ADD COLUMN is_admin boolean default false;
+
 CREATE TABLE movietick.movies (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
@@ -43,4 +46,30 @@ CREATE TABLE movietick.shows (
     show_time DATETIME NOT NULL,
     FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE,
     FOREIGN KEY (theater_id) REFERENCES theaters(id) ON DELETE CASCADE
+);
+
+CREATE TABLE movietick.seats (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    show_id BIGINT NOT NULL,
+    seat_number VARCHAR(10) NOT NULL,
+    is_booked BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (show_id) REFERENCES shows(id) ON DELETE CASCADE
+);
+
+CREATE TABLE movietick.payments (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    transaction_id VARCHAR(100) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    payment_method VARCHAR(100),
+    amount DOUBLE,
+    payment_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50)
+);
+
+CREATE TABLE movietick.payment_seat_ids (
+    payment_id BIGINT,
+    seat_id BIGINT,
+    PRIMARY KEY (payment_id, seat_id),
+    FOREIGN KEY (payment_id) REFERENCES payments(id),
+    FOREIGN KEY (seat_id) REFERENCES seats(id)
 );
